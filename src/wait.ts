959ccc4707a6e6f2ec10f0cc35db9db44ebc84b4
Nick Roussos <nick.roussos@asml.com>
2024-02-22 13:36:57 +0100

    Provide dummy implementation of wait

--- src/wait.ts
+++ src/wait.ts
@@ -5,10 +5,6 @@
  */
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
-    if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
-    }
-
-    setTimeout(() => resolve('done!'), milliseconds)
+    // implementation
   })
 }
